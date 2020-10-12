c1fcb32b6cc1c2916fb07e4e7fde6f33b49b6a31
Matheus Bonfiglioli <bonfiglioli.mat@gmail.com>
2020-10-12 16:28:57 -0300

    Added status bar color to android

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
 import Routes from './routes';
+import {StatusBar} from 'react-native';
 
 const Index = () => {
   return (
     <NavigationContainer>
+      <StatusBar backgroundColor="#47718f" barStyle="light-content" />
       <Routes />
     </NavigationContainer>
   );
